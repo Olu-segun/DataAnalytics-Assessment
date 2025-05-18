c94bed3550c339907eb8b715c3c23b1fddc18940
olu-segun <olukayodeoluseguno@gmail.com>
2025-05-18 07:47:44 +0100

    SQL data analytics assessment

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,10 +1,10 @@
 SELECT
     u.id AS customer_id,  -- Unique customer ID
     CONCAT(u.first_name, ' ', u.last_name) AS name,  -- Full name
-    TIMESTAMPDIFF(MONTH, u.date_joined, MAX(s.transaction_date)) AS tenure_months,  -- Tenure in months
+    TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,  -- Tenure in months
     COUNT(s.id) AS total_transactions,  -- Total number of transactions
     ROUND(
-        (COUNT(s.id) / NULLIF(TIMESTAMPDIFF(MONTH, u.date_joined, MAX(s.transaction_date)), 0)) * 12 
+        (COUNT(s.id) / NULLIF(TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE() ), 0)) * 12 
         * AVG(s.confirmed_amount * 0.001), 2
     ) AS estimated_clv  -- Estimated Customer Lifetime Value
 FROM users_customuser u
